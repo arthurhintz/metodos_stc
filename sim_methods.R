e9f93425103b29cf3f98480cfc8c513d83f8385e
Arthur Hintz <114536155+arthurhintz@users.noreply.github.com>
2024-11-26 21:51:54 -0300

    Add files via upload

--- sim_methods.R
+++ sim_methods.R
@@ -8,10 +8,10 @@ source("funcoes.R")
 source("methods.R")
 
 # SETs
-set.seed(1234)
-nrep <- 5
+set.seed(124)
+nrep <- 10
 mu_values <- c(0.2, 0.5, 1, 1.5)  # Valores de mu diferentes
-n <- c(30, 100, 300, 600)
+n <- c(30, 80, 300, 600)
 n_scen <- length(n)
 method <- c("MM","MLE", "AD", "MPS")  # Métodos avaliados
 
@@ -32,26 +32,32 @@ estimate_mu <- function(x, method) {
   return(result$par)  # Retorna o valor estimado de lambda
 }
 
+#==========/==========/==========/==========/==========/==========/==========/==========/
 
-# Função para simular estimativas x
-simu_methods <- function(nrep, n, mu_true, method) {
+# Simulação com todos os metodos
+simu_methods <- function(nrep, n, mu_true, methods) {
   
-  mu_estim <- numeric(nrep)
+  mu_estim <- data.frame(matrix(ncol = length(methods), nrow = nrep))
   
-  for (i in 1:nrep) {
-    sample <- rmax(n, mu_true)
-    mu_estim[i] <- estimate_mu(sample, method)
+    for (i in 1:nrep) {
+      
+      sample <- rmax(n, mu_true)
+  
+      for (k in 1:length(methods)) {
+        meth <- methods[k]
+        
+        mu_estim[i, k] <- estimate_mu(sample, meth)
+    }
   }
+  
+  colnames(mu_estim) <- methods
+  
   return(mu_estim)
 }
 
 
-#==========/==========/==========/==========/==========/==========/==========/==========/
-
-# Função principal de simulação
 run_simulation <- function(nrep, n_values, mu_values, methods) {
-
-    # DataFrame para armazenar resultados
+  
   results <- data.frame(
     n = integer(),
     mu_true = numeric(),
@@ -62,16 +68,16 @@ run_simulation <- function(nrep, n_values, mu_values, methods) {
   # Loops para tamanhos de amostra, valores de mu e métodos
   for (n in n_values) {
     for (mu in mu_values) {
+      
+      mu_estimations <- simu_methods(nrep, n, mu, methods)
+      
+      # Armazenar os resultados
       for (meth in methods) {
-
-        mu_estim <- simu_methods(nrep, n, mu, meth)
-        
-        # Armazenar os resultados 
         results <- rbind(results, data.frame(
           n = rep(n, nrep),
           mu_true = rep(mu, nrep),
           method = rep(meth, nrep),
-          mu_est = mu_estim
+          mu_est = mu_estimations[[meth]]
         ))
       }
     }
@@ -79,7 +85,6 @@ run_simulation <- function(nrep, n_values, mu_values, methods) {
   
   return(results)
 }
-
 
 results <- run_simulation(nrep, n, mu_values, method)
 
